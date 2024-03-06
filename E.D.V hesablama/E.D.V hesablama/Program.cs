69c0ba11d5730a95fe8e1baeb7f36938eb1f9e96
Vasif <159883475+ZairovVasif@users.noreply.github.com>
2024-03-06 17:03:44 +0400

    Add files via upload

---
+++ E.D.V hesablama/E.D.V hesablama/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        string reqem;
+        Console.Write("Reqem daxil edin!: ");
+        reqem = Console.ReadLine();
+        double kdv = 0.16;
+
+        Console.WriteLine("Girdiğiniz sayının %16 EDV'si: " + (Convert.ToDouble(reqem) * kdv));
+        Console.ReadKey();
+    }
+}
